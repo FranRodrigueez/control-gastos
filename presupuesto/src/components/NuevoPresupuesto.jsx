7d0bd46f2f18349d662373ce1751a12509897664
FranRodrigueez <122549243+FranRodrigueez@users.noreply.github.com>
2023-09-13 16:13:12 +0200

    16:12 13/09/2023

---
+++ presupuesto/src/components/NuevoPresupuesto.jsx
@@ -0,0 +1,25 @@
+const NuevoPresupuesto = ({presupuesto, setPresupuesto}) => {
+  return (
+    <div className="contenedor-presupuesto contenedor sombra">
+
+        <form className="formulario">
+
+            <div className="campo">
+                <label>Definir Presupuesto</label>
+                <input
+                    className="nuevo-presupuesto"
+                    type="text"
+                    placeholder="Añade tu presupuesto"
+                    value={presupuesto}
+                    onChange={e => setPresupuesto(e.target.value)}
+                />
+            </div>
+
+            <input type="submit" value="Añadir" className="boton-redondeado"/>
+
+        </form>
+    </div>
+  )
+}           
+
+export default NuevoPresupuesto
